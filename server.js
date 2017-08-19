ba2b09d2d0c5691e81bca2fdfb7ef6caa03b6d2e
basidmpk <31088722+basidmpk@users.noreply.github.com>
2017-08-19 07:34:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,8 +12,17 @@ app.get('/', function (req, res) {
 app.get('/article-one',function(req,res){
     res.send('Article  are requested and will be served here');
 });
+
+app.get('/article-one',function(req,res){
+    res.send('Article  are requested and will be served here');
+});
+
+app.get('/article-one',function(req,res){
+    res.send('Article  are requested and will be served here');
+});
+
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+ res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
