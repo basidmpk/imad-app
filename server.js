59e8faec34b01b1017aefb44ace0839158ae1ea4
basidmpk <31088722+basidmpk@users.noreply.github.com>
2017-08-21 10:20:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-    res.send(createTemplate(Article-one));
+    res.send(createTemplate(article-one));
     
 });
 
